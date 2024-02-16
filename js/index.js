219ca42d5f3e29fed4fcaa48abd85d9d1b131439
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-02-16 11:12:53 +0000

    Built site for gh-pages

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ boxIt = function(res,style) {
     cnt = "";
     res.forEach(r => {
         /* ${r.links.filter(l=>l.rel=='self').pop().href */ 
-        cnt += `<div class="g-col-6 g-col-lg-3 p-2 rounded ${style}">
+        cnt += `<div class="g-col-6 g-col-lg-3 p-3 rounded ${style}">
             <p><a href="https://kenya.lsc-hubs.org/collections/metadata:main/items/${r.id}" class="text-success fw-bold text-decoration-none">${(r.properties.title||'').substring(0,60)}</a></p>
             <p class="fs-6">${(r.properties.description||'').substring(0,150)}</p>
             </div>`})
