62326515ac3b4dc51e01c3bcf6586142357a3616
Sidney Gijzen <info@puzzlingpanda.nl>
2024-11-07 15:16:20 +0100

    prototype case page Maize

--- assets/js/getCatalogRecords.js
+++ assets/js/getCatalogRecords.js
@@ -5,18 +5,33 @@ async function getRecords({
   catalogBaseUrl,
   qs,
   numberOfItems = 6,
-  itemType,
+  itemType = '',
   icon = false, // icon name of bootstrap icon
+  keywords = '',
+  searchQuery = '',
   style = '',
-  showDescription = false
+  showDescription = false,
+  truncateTitle = false
 }) {
-  let response = await fetch(
-    `${catalogBaseUrl}/collections/metadata:main/items?f=json&limit=${numberOfItems}&sortby=-updated&type=${itemType}`
-  );
+  let url = `${catalogBaseUrl}/collections/metadata:main/items?f=json&limit=${numberOfItems}&sortby=-updated`
+
+  if (itemType) {
+    url += `&type=${itemType}`
+  }
+
+  if (keywords) {
+    url += `&keywords=${keywords}`
+  }
+
+  if (searchQuery) {
+    url += `&q=${searchQuery}`
+  }
+
+  let response = await fetch(url);
 
   if (response.ok) {
     let res = await response.json();
-    setDiv(qs, boxIt(res.features, icon, style, showDescription));
+    setDiv(qs, boxIt(res.features, icon, style, showDescription, truncateTitle));
   } else {
     console.log(response.status);
   }
@@ -28,7 +43,7 @@ setDiv = function(qs, content){
   dv.innerHTML = content;
 }
 
-boxIt = function(res, icon, style, showDescription) {
+boxIt = function(res, icon, style, showDescription, truncateTitle) {
   let cnt = "";
 
   res.forEach(r => {
@@ -41,8 +56,13 @@ boxIt = function(res, icon, style, showDescription) {
       cnt += `<i class="bi-${icon} me-2 text-primary"></i>`
     }
 
+    let title = (r.properties.title||'')
+    if (truncateTitle) {
+      title = `${title.substring(0,60)}...`
+    }
+
     cnt += `<div>
-            <a href="${linkToItem[0].href}" class="text-decoration-none">${(r.properties.title||'').substring(0,60)}</a>`
+            <a href="${linkToItem[0].href}" class="text-decoration-none">${title}</a>`
 
     if (showDescription) {
       cnt += `<p>${(r.properties.description||'').substring(0,150)}</p>`
